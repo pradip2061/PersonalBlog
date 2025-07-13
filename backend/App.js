4eca260a0274f2ff803b3fe2e3e59a183c90b230
pradip2061 <ps6000146@gmail.com>
2025-07-13 22:16:16 +0545

    fix url

--- backend/App.js
+++ backend/App.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(express.json())
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const connectToDatabase = require('./database/index');
+const createBlogRouter = require('./router/blogCreateRouter');
+const getBlogRouter = require('./router/GetBlogRouter');
+
+// Middleware
+app.use(express.json());
 app.use(cors({
-    origin:"https://personal-blog-psi-lyart.vercel.app"
-}))
-const PORT = process.env.PORT
-const connectToDatabase =require('./database/index')
-const createBlogRouter = require('./router/blogCreateRouter')
-const getBlogRouter = require('./router/GetBlogRouter')
-connectToDatabase()
-app.use('/prodevtech',createBlogRouter,getBlogRouter)
-app.listen(PORT,()=>{
-    console.log(`the project running at http://localhost:${PORT}`)
-})
+  origin:["https://personal-blog-delta-ashen.vercel.app","http://localhost:5173"],
+  credentials: true,
+}));
+
+// Connect DB
+connectToDatabase();
+
+// Routes
+app.use('/prodevtech', createBlogRouter);
+app.use('/prodevtech', getBlogRouter);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
