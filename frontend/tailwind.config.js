965c16fada4d46a8b5ac1bbbf638e438b87d2067
pradip2061 <ps6000146@gmail.com>
2025-07-15 13:24:24 +0545

    fixed issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,15 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   safelist: [
-    "text-xl",
+    "lg:text-xl",
+    "text-md",
     "font-bold",
     "dark:text-white",
     "bg-gray-300",
     "p-4",
     "text-white",
     "bg-red-500",
-    "p-10",
-    "text-black"
+    "lg:p-10",
+    "text-black",
+    "p-2"
     // Add all dynamic classes used in DB content
   ],
   darkMode: "class", // make sure dark mode is enabled
