6e1c77ee352013f5d3ddae1a592ef31709b122f7
pradip2061 <ps6000146@gmail.com>
2025-08-09 01:35:07 +0530

    fix

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -34,12 +34,12 @@ router.post("/google", async (req, res) => {
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "None",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+    // res.cookie("token", token, {
+    //   httpOnly: true,
+    //   secure: true,
+    //   sameSite: "None",
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    // });
 
     res.status(200).json({
       message: "Login successful",
